82016291b216de24bc510cee6c6cb6ba1a071016
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-04-30 11:18:59 -0400

    Hellooooo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
+import spongebob from './Spongebob.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={spongebob} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/sarai-ii"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow Me
         </a>
       </header>
     </div>
